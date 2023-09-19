0e7ae23dff1b7aa450ea7a1836b6448c729bbad7
Hasibul Hasan Shanto <kajal.shanto@gmail.com>
2023-09-19 23:36:49 +0600

    Initial uploads

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// console.log("app vue");
+</script>
+
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+// Use JS file on head like this:
+useHead({
+  link: [
+    {
+      rel: "modulepreload",
+      href: "/js/app-b4670975.js",
+    },
+    {
+      rel: "modulepreload",
+      href: "/js/tailwind.config-687e688e.js",
+    },
+    {
+      rel: "modulepreload",
+      href: "/js/tailwind.config-687e688e.js",
+    },
+    {
+      rel: "modulepreload",
+        href: "/js/apexcharts.common-2171691b.js", 
+    },
+    {
+      rel: "stylesheet",
+      href: "/css/app.css",
+    },
+  ],
+  script: [
+    {
+      type: "module",
+      src: "/js/index-57787c39.js",
+      tagPosition: "bodyClose",
+    },
+  ],
+});
+</script>
+
+<template>
+  <div>
+    <!-- navbar  -->
+    <Navbar />
+    <slot></slot>
+  </div>
+</template>
